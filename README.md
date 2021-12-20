1d502602c4d78cdd40a866fb2016dc2c1e0d5700
Elkin Narvaez <elkinnarvaez2406@gmail.com>
2021-12-20 16:44:53 -0500

    10071 solved in C++

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# OnlineJudge
+# OnlineJudge: Problem Solving
+
+## Solved Problems
+
+### Introduction
+
+#### Getting Started: The Easy Problems
+
+10071. Back to High School Physics - https://onlinejudge.org/external/100/10071.pdf
--- solutions/10071.cpp
+++ solutions/10071.cpp
@@ -1,8 +1,20 @@
+/*
+    10071. Back to High School Physics - https://onlinejudge.org/external/100/10071.pdf
+
+    Author: Elkin Narváez
+    Email: elkinnarvaez2406@gmail.com
+    Last submission: 12/15/2021
+*/
+
 #include <stdio.h>
 #include <algorithm>
 
 using namespace std;
 
 int main(){
+    int v, t;
+    while(scanf("%d %d", &v, &t) != EOF){
+        printf("%d\n", 2 * v * t);
+    }
     return 0;
 }
